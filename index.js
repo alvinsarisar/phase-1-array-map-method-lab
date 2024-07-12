8ed4d40889a727c809c2e19a17521ee8d3e3e2fb
alvin <alvin.sarisar@student.moringaschool.com>
2024-07-12 19:56:07 +0300

    map

--- index.js
+++ index.js
@@ -10,7 +10,15 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
+const exemptWords = ['OO', 'API', 'Nan', '', 'to', 'for', 'is', 'can', 'we', 'it'];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(tutorial => { //iterating through each tutorial of the array
+    return tutorial.split(' ').map(word => {//spliting the words to individual strings
+      return word.charAt(0).toUpperCase() //capitalizing first character of each word
+      + word.slice(1);//making the rest of the characters to lowwercase
+    }).join(' ');//rejoining the word strings back to the sentence string
+  });
 }
+const capital =titleCased()
+console.log (capital)
